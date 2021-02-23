817a3eaf7d421c406a253a68cdd24381f4593620
Laurent Ghigonis <ooookiwi@gmail.com>
2021-02-23 19:32:33 +0100

    add 'focus' source to stream currently focused window

--- netscreen.py
+++ netscreen.py
@@ -99,6 +99,8 @@ if args.source:
         sys.exit(0)
     elif args.source == 'select':
         window = xdo.select_window_with_click()
+    elif args.source == 'focus':
+        window = xdo.get_focused_window()
     elif args.source in monitors_list:
         monitor = monitors_list[args.source]
     elif source_int is None:
